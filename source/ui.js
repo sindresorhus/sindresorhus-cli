d4acdefb20627f9fa9d61bb9ce6301c98189f53b
Federico Brigante <github@bfred.it>
2018-05-10 16:17:21 +0300

    Linting

--- source/ui.js
+++ source/ui.js
@@ -67,7 +67,7 @@ module.exports = () => (
 	<div>
 		<br/>
 		<div>
-			<Text>{'I’m a full-time open-sourcerer making things like macOS apps, CLI tools, and modules.'}</Text>
+			<Text>I’m a full-time open-sourcerer making things like macOS apps, CLI tools, and modules.</Text>
 		</div>
 		<br/>
 		<SelectInput items={items} onSelect={handleSelect}/>
